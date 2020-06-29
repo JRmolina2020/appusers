620fed034cb8b3493f88dda79a6464c3cb60b7cc
JRMOLINA2020 <JRMOLINA2020@GMAIL.COM>
2020-06-28 21:53:07 -0500

    c2

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="user" content="{{Auth::user()}}">
-        <title>Laravel</title>
+        <title>space</title>
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{ asset('css/lte.min.css') }}">
         <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
